3b7ad91ff090d11530de727568b8865508a59328
KIWI0912 <151629154+KIWI0912@users.noreply.github.com>
2025-02-16 15:22:53 +0800

    Delete src/game directory

--- src/game/physics.py
+++
@@ -1,8 +0,0 @@
-def check_ball_collision(ball, paddle):
-    return (paddle.x <= ball.x <= paddle.x + paddle.width and
-            ball.y + ball.radius >= SCREEN_HEIGHT - paddle.height)
-
-def check_reward_collision(ball, reward):
-    return (reward.active and 
-            reward.x <= ball.x <= reward.x + reward.size and
-            reward.y <= ball.y <= reward.y + reward.size)
